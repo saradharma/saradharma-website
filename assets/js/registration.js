279ed562b56d5dabc93875f87ce81363972bfa8e
saradharma <dharm_rajan@yahoo.com>
2026-07-31 13:03:34 -0400

    Update registration.js

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -13,7 +13,7 @@
  CONFIGURATION
 ***********************************************************************/
 
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycby33ZE9UD3r-5uA8t81HdiBFWAL3imO6S4gZhSbXRECF_N4RJ07njE1oMPagsWrUs22dg/exec"
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwfkAbQLUNTucGqAaGlJ5hyCNkZ-i9C2R64RE_uq--9QUQ59aFT1h-LlU03trdvxJfHng/exec"
 
 const MIN_MESSAGE_LENGTH = 10;
 
