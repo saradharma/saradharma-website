e607b836383d0cd2a72560166c0ef885f7bd6055
saradharma <dharm_rajan@yahoo.com>
2026-07-31 03:59:02 -0400

    Update WEBAPP_URL to new endpoint

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -13,7 +13,7 @@
  CONFIGURATION
 ***********************************************************************/
 
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzFd4gLlRctpjsXu76pq8gCDOCGd9wilyYQwWZJsZMjNER1scKyCZPYO3xjnDq1hb9KGw/exec"
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwkrn-FNvKumKKKtSbNRbMzOE2V1_ZiTBjLkV34pUqDJ9l6BneOW11BBlk8N61D8FxhGA/exec"
 
 const MIN_MESSAGE_LENGTH = 100;
 
